7b1a8126d13b4198612ec15557110d2d8de2ee3c
Zachary Allwein <zrallwein@gmail.com>
2022-11-08 23:26:01 -0600

    precommit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
-const { Client, Events, GatewayIntentBits} = require('discord.js');
-const { token } = require('../config.json');
-const {isInquiry} = require('./text_proc');
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("../config.json");
+const { isInquiry, parseOpts } = require("./text_proc");
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('messageCreate', message => {
-	if(message.author.bot) return;
-	if(!isInquiry(message.content)) return;
-	message.channel.send("what is your inquiry? ");
-})
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  const content = message.content;
+
+  if (!isInquiry(content)) return;
+
+  console.log(parseOpts(content))
+
+  message.channel.send(parseOpts(content)?.toString());
+});
 // Log in to Discord with your client's token
 client.login(token);
--- src/text_proc.js
+++ src/text_proc.js
@@ -1,4 +1,6 @@
 //regex to match for strings with ??? in them
-const isInquiry = s => /.*\?\?\?*./.test(s)
+const isInquiry = (s) => /.*\?\?\?*./.test(s);
 
-module.exports = {isInquiry}
+//function to get options from inquiry
+const parseOpts = (s) => s.match(/\s-\w+/g);
+module.exports = { isInquiry, parseOpts};
